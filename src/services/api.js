7849bb1b979932108a1430b5e8df761707736e34
J Siddharth <117640763+SiddharthSJ15@users.noreply.github.com>
2025-10-20 00:49:32 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/movies/";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getMovies = async () => {
   return await axios.get(API_URL);
 };
 
 export const getMovie = async (id) => {
-  return await axios.get(`${API_URL}${id}/`);
+  return await axios.get(`${API_URL}/api/movies/${id}/`);
 };
 
 export const addMovie = async (data) => {
-  return await axios.post(`${API_URL}add/`, data);
+  return await axios.post(`${API_URL}/api/movies/add/`, data);
 };
 
 export const updateMovie = async (id, data) => {
-  return await axios.put(`${API_URL}${id}/update/`, data);
+  return await axios.put(`${API_URL}${id}/api/movies/update/`, data);
 };
 
 export const deleteMovie = async (id) => {
-  return await axios.delete(`${API_URL}${id}/delete/`);
+  return await axios.delete(`${API_URL}${id}/api/movies/delete/`);
 };

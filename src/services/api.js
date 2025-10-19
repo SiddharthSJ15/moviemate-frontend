e3d19c6a765dff40712e4e9fed929c5730ce062c
J Siddharth <117640763+SiddharthSJ15@users.noreply.github.com>
2025-10-20 01:41:22 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const getMovies = async () => {
-  return await axios.get(API_URL/api/movies);
+  return await axios.get(`${API_URL}/api/movies`);
 };
 
 export const getMovie = async (id) => {
